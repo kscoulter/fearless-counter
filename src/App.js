b3912a8398187dcd1d4adc1ffb2086da6770961a
Ksenia Coulter <kzherdeva@ucdavis.edu>
2022-09-21 11:43:44 -0400

    hit counter api to display accurate count

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 
+
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import './App.css';
 import Button from './components/Button';
 import CountDisplay from './components/CountDisplay';
 
-function App() {
+// not a secret
+const key = '1ccb732e-b55a-4404-ad3f-0f99c02fe44e'
+const namespace = 'fearless.tech'
+
+const App = () => {
+  const [fearlessCounter, setFearlessCounter] = useState(1)
+
+  useEffect(() => {
+    getCount()
+  }, [])
+
+  const getCount = () => {
+    axios
+      .get(`https://api.countapi.xyz/get/${namespace}/${key}`)
+      .then((res) => {
+        setFearlessCounter(res.data.value)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  
+  const incrementCount = () => {
+    axios
+      .get(`https://api.countapi.xyz/hit/${namespace}/${key}`)
+      .then((res) => {
+        setFearlessCounter(res.data.value)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <CountDisplay count="1" />
-        <Button text="click me" onClick={() => console.log("clicked")}/>
+        <CountDisplay count={fearlessCounter} />
+        <Button text="click me" onClick={incrementCount}/>
       </header>
     </div>
   );

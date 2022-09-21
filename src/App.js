65de8bfcc757c17e7dcf2e2b5985b6a8222b4537
Ksenia Coulter <kzherdeva@ucdavis.edu>
2022-09-21 11:04:04 -0400

    make simple display component for the counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
-import Button from './components/button';
+import Button from './components/Button';
+import CountDisplay from './components/CountDisplay';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CountDisplay count="1" />
         <Button text="click me" onClick={() => console.log("clicked")}/>
       </header>
     </div>
---
+++ src/components/CountDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CountDisplay = ({count}) => (
+    <div className='countDisplay'>{count}</div>
+)
+
+export default CountDisplay;
